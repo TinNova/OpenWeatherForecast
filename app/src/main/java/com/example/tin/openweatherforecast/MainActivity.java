9d5073096e90b036f852a38f93c04b7d121954d5
Tin <tinnovamail@gmail.com>
2018-05-06 11:29:42 +0100

    feat: RecyclerView and Adapter added to MainActivity
    The recyclerView and Adapter elements have been added to the
    MainActivity. However the WeatherAdapter has not been created yet,
    therefore the code is not functioning yet.

--- app/src/main/java/com/example/tin/openweatherforecast/MainActivity.java
+++ app/src/main/java/com/example/tin/openweatherforecast/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tin.openweatherforecast;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
@@ -21,15 +22,33 @@ public class MainActivity extends AppCompatActivity {
      * Needed for the RecyclerView
      */
     private RecyclerView mRecyclerView;
-    private WeatherAdapter mWeatherAdapter;
+    private RecyclerView.Adapter mAdapter;
     // Public because it is used in CompanyDetailActivity to addToDatabase
-    public ArrayList<Weather> mWeather = new ArrayList<>();
+    private ArrayList<Weather> mWeather;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Creating The RecyclerView */
+        // This will be used to attach the RecyclerView to the MovieAdapter
+        mRecyclerView = (RecyclerView) findViewById(R.id.rV_weatherList);
+        // This will improve performance by stating that changes in the content will not change
+        // the child layout size in the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+        /*
+         * A LayoutManager is responsible for measuring and positioning item views within a
+         * RecyclerView as well as determining the policy for when to recycle item views that
+         * are no longer visible to the user.
+         */
+        LinearLayoutManager mLinearLayoutManager =
+                new LinearLayoutManager(this);
+        // Set the mRecyclerView to the layoutManager so it can handle the positioning of the items
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        // Creating the mTheCompanies ArrayList<> to avoid a null exception
+        mWeather = new ArrayList<>();
 
         //TODO: Check for an internet connection first, if none, then, if SQL data is less than
         //TODO:... 24hrs old display it, else display a no data screen.
@@ -47,11 +66,18 @@ public class MainActivity extends AppCompatActivity {
             NetworkConnection.getInstance(this).getResponseFromHttpUrl(weatherRequestUrl, new NetworkListener() {
                 @Override
                 public void getWeatherArrayList(ArrayList<Weather> weather) {
-
+                    // Logging the weather ArrayList to see if it's functioning
                     Log.i(TAG, "ArrayList Weather: " + weather);
 
-                    // This will efficiently update the adapter with new information
-                    adapter.notifyDataSetChanged();
+                    mAdapter = new WeatherAdapter(mWeather, getApplicationContext(), MainActivity.this);
+                    mRecyclerView.setAdapter(mAdapter);
+
+                    /*
+                    * Used to update the adapter when information is there already, for example
+                    * if the SQLite data present, and user conencts to wifi, we want the latest
+                    * data to overwrite the SQLite data in the adapter and recyclerView
+                    */
+                    mAdapter.notifyDataSetChanged();
 
                 }
             });
